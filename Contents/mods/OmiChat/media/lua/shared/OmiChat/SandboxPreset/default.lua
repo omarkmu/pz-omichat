585f4c070012c22e2ee77e3f3e99deb5267328b2
Omar <omarkmu@gmail.com>
2024-09-20 22:18:23 -0500

    Add compatibility patch for buffy's character bios

--- Contents/mods/OmiChat/media/lua/shared/OmiChat/SandboxPreset/default.lua
+++ Contents/mods/OmiChat/media/lua/shared/OmiChat/SandboxPreset/default.lua
@@ -11,6 +11,7 @@ return {
     EnableCleanCharacter = 3,
     EnableSetName = 2,
     EnableDiscordColorOption = 3,
+    EnableCompatBuffyCharacterBios = 3,
     EnableCompatBuffyRPGSystem = 2,
     EnableCompatChatBubble = 3,
     EnableCompatSearchPlayers = 3,
